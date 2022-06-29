ccfa5326f993af76433f65cd8e6166b022031363
Лютый прогер / <83010538+TheMuvin@users.noreply.github.com>
2022-06-29 08:04:23 +0500

    Add files via upload

--- script_cliker.js
+++ script_cliker.js
@@ -2,18 +2,17 @@ const btn_click = document.querySelector(".btn");
 btn_click.addEventListener("click", game);
 var click_x = 0;
 var tablo = document.querySelector("#tablo_click");
-
+var cliker_plus = document.querySelector("#Clicker_plus");
+var clicks = 1;
 function game() {
-  if (click_x == 100) {
-    click_x = click_x - 100;
-    tablo.textContent = click_x;
-  } else {
-    click_x = click_x + 1;
+  if (click_x <= 99) {
+    click_x = click_x + clicks;
     tablo.textContent = click_x;
   }
-  function sound() {
-    var audio = new Audio();
-    audio.src = '6a6ad928cfac061.mp3';
-    audio.autoplay = true;
- }
+  if (click_x >= 100 && click_x <= 199) {
+    clicks = 2;
+    click_x = click_x + clicks;
+    cliker_plus.textContent = clicks;
+    tablo.textContent = click_x;
+  }
 }
