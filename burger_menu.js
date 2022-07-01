a4a775a34e6b5ae52f7f56b7854b682a8d0f3f4b
Лютый прогер / <83010538+TheMuvin@users.noreply.github.com>
2022-07-02 00:03:09 +0500

    Бургер готов

---
+++ burger_menu.js
@@ -0,0 +1,13 @@
+const burger_click2 = document.querySelector(".mobile_burger");
+const burger_click1 = document.querySelector(".mobile_burger_menu");
+burger_click2.addEventListener("click", open);
+burger_click1.addEventListener("click", close);
+var ac = document.getElementById("menu");
+function open() {
+  ac.classList.remove("none");
+  console.log(active, ac);
+}
+function close() {
+  ac.classList.add("none");
+  console.log(active, ac);
+}
